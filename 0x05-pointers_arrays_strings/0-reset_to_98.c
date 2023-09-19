81a66e8b542d33e4b634529ed804f9d9886765b0
Princehabeeb <habiy3400@gmail.com>
2023-09-18 22:57:50 -0700

    new

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,6 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
+void reset_to_98(int *n)
 {
-  int n;
-  int *reset_to_98;
-
-  n = 98;
-  p = &n;
-  printf("Address of 'n': %p\n", &n);
-  printf("Value of 'p': %p\n", p);
-  return (0);
+  *n = 98;
 }
