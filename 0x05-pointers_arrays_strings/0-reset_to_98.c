ee30dcd1cd46820eadd48598cb3f5b3d4bb5ed29
Princehabeeb <habiy3400@gmail.com>
2023-09-18 22:46:51 -0700

    new

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int *reset_to_98;
+
+  n = 98;
+  p = &n;
+  printf("Address of 'n': %p\n", &n);
+  printf("Value of 'p': %p\n", p);
+  return (0);
+}
